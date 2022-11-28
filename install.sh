48acc64d98aaf6cdee4d01e5fd9808f258e0b639
Tobias Gurtzick <magic@wizardtales.com>
2022-11-28 19:03:49 +0100

    add unattended vim install
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- install.sh
+++ install.sh
@@ -48,4 +48,5 @@ printf "Adding python dependencies"
 printf "Add neovim plugin manager"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim +'PlugInstall --sync' +qa
+nvim --headless +'echoerr 1' +'PlugInstall --sync' +qa
+nvim +UpdateRemotePlugins +qa
